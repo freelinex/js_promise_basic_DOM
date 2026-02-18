2b78ae15a97cccc3c62a0c3e9e940bc908c40148
freelinex <freelinex7@gmail.com>
2026-02-19 00:24:01 +0200

    correcting remarks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,31 +3,34 @@
 const logo = document.querySelector('.logo');
 
 const promise1 = new Promise((resolve, reject) => {
-  if (!logo) {
-    return;
+  if (logo) {
+    logo.addEventListener('click', () => {
+      resolve();
+    });
+  } else {
+    reject(new Error('no data'));
   }
-
-  logo.addEventListener('click', () => {
-    resolve();
-  });
 });
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(reject, 3000);
 });
 
-promise1.then(() => {
+const resolved = () => {
   const div = document.createElement('div');
 
   div.className = 'message';
   div.textContent = 'Promise was resolved!';
-  logo.before(div);
-});
+  document.body.append(div);
+};
 
-promise2.catch(() => {
+const rejected = () => {
   const div = document.createElement('div');
 
   div.className = 'message error-message';
   div.textContent = 'Promise was rejected!';
-  logo.before(div);
-});
+  document.body.append(div);
+};
+
+promise1.then(resolved).catch(rejected);
+promise2.then(resolved).catch(rejected);
